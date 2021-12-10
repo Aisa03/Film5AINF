0e053d461bf33c38fca7cfbb72235be04f2546a1
Aisa03 <alessioaisa03@gmail.com>
2021-12-10 14:40:23 +0100

    bottone

--- app/src/main/java/com/example/film5ainf/MainActivity.java
+++ app/src/main/java/com/example/film5ainf/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.film5ainf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvfilm;
     String film[] = {"Spiderman", "Titanic", "Batman"};
+    Button btnesci;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         lvfilm = (ListView) findViewById(R.id.lvFilm);
         ArrayAdapter <String> aaFilm = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, film);
         lvfilm.setAdapter(aaFilm);
+
+        btnesci = (Button) findViewById(R.id.btnesci);
+        btnesci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Vuoi uscire", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+    //abbiamo creato un contenitore che viene utilizzato per essere popolato da dei dati (arrayadapter)
+    //in particolare lo usiamo per la listview
+
+    //ascoltatore, porre un oggetto in ascolto continuo sugli effetti che avvengono su di lui
+    //l'ascoltatore permette di controllare meglio la situazione.
 }
