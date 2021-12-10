624843943d0fd00dc9508dfb62f4fe4c4e03cc4f
Aisa03 <alessioaisa03@gmail.com>
2021-12-10 14:09:30 +0100

    commit iniziale

--- app/src/main/java/com/example/film5ainf/MainActivity.java
+++ app/src/main/java/com/example/film5ainf/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.film5ainf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lvfilm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvfilm = (ListView) findViewById(R.id.lvFilm);
+
     }
 }
