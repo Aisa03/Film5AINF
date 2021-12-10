2d445e408d9332ee7d386e5993585d8c1144878a
Aisa03 <alessioaisa03@gmail.com>
2021-12-10 14:16:23 +0100

    aggiunta array adapter

--- app/src/main/java/com/example/film5ainf/MainActivity.java
+++ app/src/main/java/com/example/film5ainf/MainActivity.java
@@ -3,17 +3,20 @@ package com.example.film5ainf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvfilm;
+    String film[] = {"Spiderman", "Titanic", "Batman"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvfilm = (ListView) findViewById(R.id.lvFilm);
+        ArrayAdapter <String> aaFilm = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, film);
 
     }
 }
